5aa2327fddf512ecbd5543ffd32f92273e0a6fc2
akghule <akshayghule1110@gmail.com>
2023-02-16 23:00:36 +0530

    update assignment
    update assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,16 @@
+console.log("************* step 1  ************");
+var myFullName;
+console.log(myFullName);
+var myFullName="Akshay Ghule";
+console.log(myFullName);
+var myFullName="Akshay Somnath Ghule"
+console.log(myFullName);
+
+console.log("**************  Step 2  ****************");
+ var myLove ="I Love Only JS";
+ console.log(myLove);
+
+
+ console.log("**************  Step 3  ****************");
+ var name1=" firstName:akshay, lastName:ghule, age:25,address:beed,pincode:431519 ,gender: male ,ismarried:no"
+console.log(name1);
--- helloWorld.js
+++ helloWorld.js
@@ -2,3 +2,7 @@ console.log("Akshay Ghule");
 console.log(32);
 console.log(30*5);
 console.log(30+5-3);
+var fullName="Akshay Ghule"
+console.log(fullName);
+var city;
+console.log(city);
