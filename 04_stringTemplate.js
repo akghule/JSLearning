10873b2771ad96d9fbe062480e8449b30e9647c4
akghule <akshayghule1110@gmail.com>
2023-02-22 10:38:48 +0530

    update
    update

--- 04_stringTemplate.js
+++ 04_stringTemplate.js
@@ -3,7 +3,5 @@ var firstName="akshay";
 var lastName="ghule"
 city="pune";
 
-
-
 console.log("My First name ", firstName, "Last name ", lastName, " City ", city);
 console.log(`my first name ${firstName} last name ${lastName} city ${city}`);
