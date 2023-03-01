5c8d693943f8ed30569122b8752edfe90c476c8a
akghule <akshayghule1110@gmail.com>
2023-03-01 11:15:02 +0530

    update
    update

---
+++ 05_incremnetDecrementOperator.js
@@ -0,0 +1,5 @@
+var index=10;
+console.log(index);
+
+index=++index;
+console.log(index);
---
+++ 06_forLoop.js
@@ -0,0 +1,11 @@
+console.log(`Table of 5`);
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+
+console.log(`*************************`);
+for (let index = 5; index <= 50; ) {
+    console.log(index);
+    
+}
